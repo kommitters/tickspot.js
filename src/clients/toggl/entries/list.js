6ab29accb19983837e05751d0759d75d94d55c9a
Mario <64106728+MarioRodriguezS@users.noreply.github.com>
2021-10-28 17:45:32 -0500

    define the async function in list.js

--- src/clients/toggl/entries/list.js
+++ src/clients/toggl/entries/list.js
@@ -3,7 +3,7 @@ import { TOGGL_API_TOKEN, TOGGL_BASE_URL } from 'toggltick-js/config';
 
 const URL = `${TOGGL_BASE_URL}/time_entries`;
 
-const getTogglEntries = async (startDate, endDate) => {
+export default async function getTogglEntries(startDate, endDate) {
   const response = await axios.get(URL, {
     auth: { username: TOGGL_API_TOKEN, password: 'api_token' },
     params: { start_date: startDate, end_date: endDate },
@@ -12,6 +12,4 @@ const getTogglEntries = async (startDate, endDate) => {
 
   if (response?.data) return response.data;
   return [];
-};
-
-export default { getTogglEntries };
+}
