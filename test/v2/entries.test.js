5b29709f74baa11da872fdfbe02ad36c39c5132e
Mario <64106728+MarioRodriguezS@users.noreply.github.com>
2021-12-27 12:34:41 -0500

    add tests to client and index

--- test/v2/entries.test.js
+++ test/v2/entries.test.js
@@ -13,7 +13,6 @@ describe('createTickEntries', () => {
     notes: 'Issue',
     taskId: 14356973,
   };
-  const dataEntryMissed = { ...dataEntry, hours: null };
 
   describe('when API call is successful', () => {
     const succesfulResponse = createResponse(dataEntry, 'succesful');
@@ -44,12 +43,22 @@ describe('createTickEntries', () => {
       expect(response).toBe(authenticationError.response.data);
     });
 
-    it('Should reject with an error when required data missed', async () => {
+    it('Should reject with an error when hours data missed', async () => {
+      const dataEntryMissed = { ...dataEntry, hours: null };
       const dataMissedError = createResponse(dataEntryMissed, 'dataMissedError');
       axios.post.mockRejectedValue(dataMissedError);
-      const response = await client.entries.create({ ...dataEntryMissed, hours: null });
+      const response = await client.entries.create(dataEntryMissed);
 
       expect(response).toEqual(new Error('hours field is missing'));
+    });
+
+    it('Should reject with an error when taskId data missed', async () => {
+      const dataEntryMissed = { ...dataEntry, taskId: null };
+      const dataMissedError = createResponse(dataEntryMissed, 'dataMissedError');
+      axios.post.mockRejectedValue(dataMissedError);
+      const response = await client.entries.create(dataEntryMissed);
+
+      expect(response).toEqual(new Error('taskId field is missing'));
     });
   });
 });
