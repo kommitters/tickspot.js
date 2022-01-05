1ff2845400733a3aa4734ceb1adcce32b3782c42
Juan Gaviria <juan.gaviria@kommit.co>
2022-01-04 20:51:06 -0500

    Create getEntry method

--- test/v2/entries.test.js
+++ test/v2/entries.test.js
@@ -62,3 +62,64 @@ describe('createTickEntries', () => {
     });
   });
 });
+
+describe('getEntry', () => {
+  describe('when API call is successful', () => {
+    const responseData = createResponse({}, 'getEntrySuccesful');
+
+    beforeEach(() => {
+      axios.get.mockResolvedValueOnce(responseData);
+    });
+
+    it('should return the entry information', async () => {
+      const response = await client.entries.getEntry('123456');
+
+      expect(axios.get).toHaveBeenCalledTimes(1);
+      expect(axios.get).toHaveBeenCalledWith(
+        'https://www.tickspot.com/123456/api/v2/entries/123456.json',
+        {
+          headers: {
+            Authorization: 'Token token=ar425598573462y24ec1ceee728981663',
+            'User-Agent': 'tickspot.js (user@kommit.co)',
+          },
+        },
+      );
+
+      expect(response).toEqual(responseData.data);
+    });
+  });
+
+  describe('When API call does not find a resource', () => {
+    const responseData = createResponse({}, 'getEntryNotFound');
+
+    beforeEach(() => {
+      axios.get.mockResolvedValueOnce(responseData);
+    });
+
+    it('should return 404 status', async () => {
+      const response = await client.entries.getEntry('123456');
+
+      expect(axios.get).toHaveBeenCalledWith(
+        'https://www.tickspot.com/123456/api/v2/entries/123456.json',
+        {
+          headers: {
+            Authorization: 'Token token=ar425598573462y24ec1ceee728981663',
+            'User-Agent': 'tickspot.js (user@kommit.co)',
+          },
+        },
+      );
+
+      expect(response.data.status).toEqual(404);
+    });
+  });
+
+  describe('when getEntry method returns an error', () => {
+    it('should reject with an error because of missing parameters', async () => {
+      try {
+        await client.entries.getEntry();
+      } catch (error) {
+        expect(error).toEqual(new Error('entryId field is missing'));
+      }
+    });
+  });
+});
--- test/v2/fixture/entries/createResponse.js
+++ test/v2/fixture/entries/createResponse.js
@@ -1,7 +1,8 @@
 import responseGenerator from '../responseGenerator.js';
-import dataSuccessful from './responseData.js';
+import { dataSuccessful, dataEntrySuccessful, dataEntryNotFound } from './responseData.js';
 
 const Url = 'https://www.tickspot.com/114217/api/v2/entries.json';
+const getEntryUrl = 'https://www.tickspot.com/114217/api/v2/entries/123456.json';
 const messageTimeMissed = { errors: 'field is missing' };
 const auth = 'Token token=12345akeu785asd45ac48';
 
@@ -9,6 +10,14 @@ const createResponse = (dataEntry, responseType) => {
   switch (responseType) {
     case 'succesful':
       return responseGenerator(Url, 201, 'Created', 'post', dataEntry, dataSuccessful, auth);
+
+    case 'getEntrySuccesful':
+      return responseGenerator(getEntryUrl, 200, 'OK',
+        'get', dataEntry, dataEntrySuccessful, auth);
+
+    case 'getEntryNotFound':
+      return responseGenerator(getEntryUrl, 404, 'Not Found',
+        'get', dataEntry, dataEntryNotFound, auth);
 
     case 'authenticationError':
       return {
