3102535dc277ca25788be26909a4cd2074f737aa
Juan Gaviria <juan.gaviria@kommit.co>
2022-01-19 20:49:56 -0500

    Restructure test folder and files

---
+++ test/v2/createEntries.test.js
@@ -0,0 +1,71 @@
+import axios from 'axios';
+import tickspot from '#src/index';
+import responseFactory from '#test/v2/fixture/responseFactory';
+import userInfo from '#test/v2/fixture/client';
+import dataSuccessful from './fixture/entries/createResponseData.js';
+
+jest.mock('axios');
+const client = tickspot({ apiVersion: 2, ...userInfo });
+const createEntriesUrl = 'https://www.tickspot.com/114217/api/v2/entries.json';
+
+describe('createTickEntries', () => {
+  const dataEntry = {
+    date: '2021-11-08',
+    hours: 2,
+    notes: 'Issue',
+    taskId: 14356973,
+  };
+
+  describe('when API call is successful', () => {
+    const succesfulResponse = responseFactory(dataEntry, 'create',
+      dataSuccessful, createEntriesUrl);
+
+    beforeEach(() => {
+      axios.post.mockResolvedValueOnce(succesfulResponse);
+    });
+
+    it('should return the tick data entry', async () => {
+      const response = await client.entries.create(dataEntry);
+
+      expect(response).toBe(succesfulResponse.data);
+    });
+
+    it('should create the tick entry', async () => {
+      const response = await client.entries.create(dataEntry);
+
+      expect(response.data.date).toBe(dataEntry.date);
+      expect(response.data.notes).toBe(dataEntry.notes);
+    });
+  });
+
+  describe('when create method returns an error', () => {
+    it('Should reject with an error when authentication fails', async () => {
+      const authenticationError = responseFactory(dataEntry, 'authenticationError',
+        {}, createEntriesUrl);
+      axios.post.mockRejectedValueOnce(authenticationError);
+      const response = await client.entries.create(dataEntry);
+
+      expect(response).toBe(authenticationError.response.data);
+    });
+
+    it('Should reject with an error when hours data missed', async () => {
+      const dataEntryMissed = { ...dataEntry, hours: null };
+      const dataMissedError = responseFactory(dataEntryMissed, 'dataMissedError',
+        {}, createEntriesUrl);
+      axios.post.mockRejectedValue(dataMissedError);
+      const response = await client.entries.create(dataEntryMissed);
+
+      expect(response).toEqual(new Error('hours field is missing'));
+    });
+
+    it('Should reject with an error when taskId data missed', async () => {
+      const dataEntryMissed = { ...dataEntry, taskId: null };
+      const dataMissedError = responseFactory(dataEntryMissed, 'dataMissedError',
+        {}, createEntriesUrl);
+      axios.post.mockRejectedValue(dataMissedError);
+      const response = await client.entries.create(dataEntryMissed);
+
+      expect(response).toEqual(new Error('taskId field is missing'));
+    });
+  });
+});
--- test/v2/fixture/entries/getEntryResponseData.js
+++ test/v2/fixture/entries/getEntryResponseData.js
@@ -1,17 +1,3 @@
-export const dataSuccessful = {
-  data: {
-    id: 99547865,
-    date: '2021-11-08',
-    hours: 2,
-    notes: 'Issue',
-    task_id: 14521973,
-    user_id: 377264,
-    url: 'https://secure.tickspot.com/114217/api/v2/entries.json',
-    created_at: '2021-11-09T11:30:40.000-05:00',
-    updated_at: '2021-11-09T11:30:40.000-05:00',
-  },
-};
-
 export const dataEntrySuccessful = {
   data: {
     id: 123456,
@@ -45,4 +31,4 @@ export const dataEntryNotFound = {
   },
 };
 
-export default { dataSuccessful, dataEntrySuccessful, dataEntryNotFound };
+export default { dataEntrySuccessful, dataEntryNotFound };
