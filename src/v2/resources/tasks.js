728ab41421c8c7041beb66c36b129db098f10656
Esteban Córdoba <esteban.cordoba@moove-it.com>
2022-02-09 22:26:03 -0500

    Update task function [#40]

--- src/v2/resources/tasks.js
+++ src/v2/resources/tasks.js
@@ -16,6 +16,49 @@ class Tasks extends BaseResource {
     const URL = `${this.baseURL}/tasks/closed.json`;
     return this.makeRequest({ URL, method: 'get', responseCallback });
   }
+
+  /**
+   * This method will update a specified task, it is strictly limited to administrators.
+   *
+   * @param {object} Task contains the params to update the task.
+   *    [Required] taskId
+   *    [Required] budget
+   *    [Optional] billable
+   *    [Optional] name
+   *    [Optional] position
+   *    [Optional] projectId
+   *    [Optional] dateClosed
+   * @param {callback} responseCallback
+   *    is an optional function to perform a process over the response data.
+   *
+   * @return {Object} a object with updated task data.
+   */
+  async update({
+    taskId,
+    budget,
+    billable,
+    name,
+    position,
+    projectId,
+    dateClosed,
+  }, responseCallback) {
+    if (!taskId) throw new Error('taskId field is missing');
+    if (budget === undefined) throw new Error('budget field cannot be undefined');
+
+    const body = {
+      budget,
+      ...(typeof billable === 'boolean' && { billable }),
+      ...(name && { name }),
+      ...(position && { position }),
+      ...(projectId && { project_id: projectId }),
+      ...(dateClosed && { date_closed: dateClosed }),
+    };
+
+    const URL = `${this.baseURL}/tasks/${taskId}.json`;
+    return this.makeRequest({
+      URL, method: 'put', body, responseCallback,
+    });
+  }
 }
 
 export default Tasks;
