b1f226b11e7753d479e1feec87f6e77ac4764a3a
brayvasq <brayvasq@gmail.com>
2022-05-16 18:42:39 -0500

    (#20) Change how to instanciate the lib to avoid confusions   * Update documentation

--- test/tickspot.test.js
+++ test/tickspot.test.js
@@ -1,15 +1,15 @@
-import tickspot from '#src/index';
+import Tickspot from '#src/index';
 import userInfo from '#test/v2/fixture/client';
 import Client from '#src/v2/client';
 
-describe('tickspot', () => {
+describe('Tickspot.init', () => {
   it('when the tickspot method returns the client', () => {
-    const client = tickspot({ apiVersion: 2, ...userInfo });
+    const client = Tickspot.init({ apiVersion: 2, ...userInfo });
     expect(client).toBeInstanceOf(Client);
   });
 
   it('when the api version is not available', () => {
-    const client = tickspot({ apiVersion: 3, ...userInfo });
+    const client = Tickspot.init({ apiVersion: 3, ...userInfo });
     expect(client).toEqual(Error('The version is not available'));
   });
 });
